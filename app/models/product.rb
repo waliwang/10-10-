65f6a20155c277daaa6befa1b1bed9ea7a27eb06
wali <larts393@gmail.com>
2021-10-17 23:18:05 +0800

    必填選項

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,6 +4,7 @@ class Product < ApplicationRecord
 
     validates :name, presence: true
     validates :description, presence: true
+    
 end
 
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
     has_one :store
+
+    validates :email, presence: true
+    validates :name, presence: true
+    
 end
